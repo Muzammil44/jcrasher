8b5e8f9a83e7106ec9dbc228fb65401ca789ba50
csallner <csallner@f7bbb36b-3a1e-0410-9594-3503ff4f9587>
2006-10-08 23:34:20 +0000

    - Missing files.

---
+++ examples/trivia/DivByZero.java
@@ -0,0 +1,21 @@
+/*
+ * DivByZero.java
+ * 
+ * Copyright 2006 Christoph Csallner and Yannis Smaragdakis.
+ */
+package trivia;
+
+/**
+ * Throws java.lang.ArithmeticException: / by zero
+ * 
+ * @author csallner@gatech.edu (Christoph Csallner)
+ */
+public class DivByZero {
+
+	/**
+	 * Crashes for i==0.
+	 */
+  public static double inverse(int i) {	
+		return 1/i;
+	}	
+}
