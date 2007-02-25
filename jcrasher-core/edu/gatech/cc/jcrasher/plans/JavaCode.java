f8323d51e112230e34dc24e0401e80879d767e89
csallner <csallner@f7bbb36b-3a1e-0410-9594-3503ff4f9587>
2007-02-25 23:48:43 +0000

    Renamed and added text() interface, which is a synonym for toString().

---
+++ jcrasher-core/edu/gatech/cc/jcrasher/plans/JavaCode.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2006 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.gatech.cc.jcrasher.plans;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Junk of Java code.
+ * We can execute this junk of code  via Java reflection
+ * or produce a textual representation.
+ * 
+ * @param <T> result type.
+ * 
+ * @author csallner@gatech.edu (Christoph Csallner)
+ */
+public interface JavaCode<T> {
+  
+  /**
+   * To be called from above, like TestCaseGenerator.
+   * 
+   * First executes all of its parameters and then itself on the
+   * parameter values.
+   * 
+   * @return maybe null.
+   */
+  public T execute() throws InstantiationException,
+      IllegalAccessException, InvocationTargetException;
+  
+  
+  /**
+   * @return textual representation of code junk.
+   */
+  public String text();
+}
