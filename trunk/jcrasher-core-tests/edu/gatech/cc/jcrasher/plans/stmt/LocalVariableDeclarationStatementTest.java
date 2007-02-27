669e4f9caab30735d5296e0c0a9a9f9d627fe4ff
csallner <csallner@f7bbb36b-3a1e-0410-9594-3503ff4f9587>
2007-02-27 03:37:26 +0000

    Reflected new package hierarchy in test classes.

--- trunk/jcrasher-core-tests/edu/gatech/cc/jcrasher/plans/stmt/LocalVariableDeclarationStatementTest.java
+++ trunk/jcrasher-core-tests/edu/gatech/cc/jcrasher/plans/stmt/LocalVariableDeclarationStatementTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package edu.gatech.cc.jcrasher.plans.blocks;
+package edu.gatech.cc.jcrasher.plans.stmt;
 
 
 import junit.framework.TestCase;
